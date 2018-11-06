c89f60ac923d370120a920a4652b6271add243a3
Gregory <gregory.caron1@gmail.com>
2018-11-06 09:18:19 +0100

    Corrections

--- script.js
+++ script.js
@@ -1,5 +1,56 @@
-let fNum = Math.round(Math.random() * 10);
-let sNum = Math.round(Math.random() * 10);
+let fNum;
+let sNum;
+
+function generateNumb() {
+    let tabTable = [];
+    let retVal;
+    if (document.getElementById('0').checked === true) {
+        tabTable.push(0);
+    }
+    if (document.getElementById('1').checked === true) {
+        tabTable.push(1);
+    }
+    if (document.getElementById('2').checked === true) {
+        tabTable.push(2);
+    }
+    if (document.getElementById('3').checked === true) {
+        tabTable.push(3);
+    }
+    if (document.getElementById('4').checked === true) {
+        tabTable.push(4);
+    }
+    if (document.getElementById('5').checked === true) {
+        tabTable.push(5);
+    }
+    if (document.getElementById('6').checked === true) {
+        tabTable.push(6);
+    }
+    if (document.getElementById('7').checked === true) {
+        tabTable.push(7);
+    }
+    if (document.getElementById('8').checked === true) {
+        tabTable.push(8);
+    }
+    if (document.getElementById('9').checked === true) {
+        tabTable.push(9);
+    }
+    if (document.getElementById('10').checked === true) {
+        tabTable.push(10);
+    }
+    if (tabTable.length > 0) {
+        retVal = Math.floor(Math.random() * tabTable.length);
+        console.log('éléments dans le tableau : ' + tabTable.length + ', sélectionné : ' + retVal + ', valeur : ' + parseInt(tabTable[retVal]));
+        retVal = parseInt(tabTable[retVal]);
+    } else {
+        alert('Sélectionne au moins une table !');
+        retVal = 0;
+    }
+    return retVal;
+}
+
+//fNum = Math.round(Math.random() * 10);
+fNum = generateNumb();
+sNum = Math.round(Math.random() * 10);
 document.getElementById('op').innerHTML = fNum + ' x ' + sNum;
 document.getElementById('champs').value ='';
 document.getElementById('statut').innerHTML = '';
@@ -8,23 +59,27 @@ document.getElementById('champs').focus();
 function combien() {
     let rNum = fNum * sNum;
     let hNum = document.getElementById('champs').value;
-    hNum = Number(hNum);
-    if (document.getElementById('champs').value !== '') {
-        if (hNum === rNum) {
-            document.getElementById('resultat').innerHTML = document.getElementById('resultat').innerHTML + '<br><span id="bon">' + fNum + ' x ' + sNum + ' = ' + rNum + '</span>';
-            document.getElementById('statut').innerHTML = 'Gagné !';
-            fNum = Math.round(Math.random() * 10);
-            sNum = Math.round(Math.random() * 10);
-            document.getElementById('op').innerHTML = fNum + ' x ' + sNum;
-        } else {
-            document.getElementById('resultat').innerHTML = document.getElementById('resultat').innerHTML + '<br><span id="pasbon">' + fNum + ' x ' + sNum + ' = ' + hNum + '</span>';
-            document.getElementById('statut').innerHTML = 'Perdu !';
+    hNum = parseInt(hNum);
+    if (Number.isInteger(hNum) === true){
+        if (document.getElementById('champs').value !== '') {
+            if (hNum === rNum) {
+                document.getElementById('resultat').innerHTML = document.getElementById('resultat').innerHTML + '<br><span id="bon">' + fNum + ' x ' + sNum + ' = ' + rNum + '</span>';
+                document.getElementById('statut').innerHTML = 'Gagné !';
+                fNum = generateNumb();
+                //fNum = Math.round(Math.random() * 10);
+                sNum = Math.round(Math.random() * 10);
+                document.getElementById('op').innerHTML = fNum + ' x ' + sNum;
+            } else {
+                document.getElementById('resultat').innerHTML = document.getElementById('resultat').innerHTML + '<br><span id="pasbon">' + fNum + ' x ' + sNum + ' = ' + hNum + '</span>';
+                document.getElementById('statut').innerHTML = 'Perdu !';
 
+            }
         }
     }
     document.getElementById('champs').value ='';
     document.getElementById('champs').focus();
 }
+
 function zero() {
     document.getElementById('resultat').innerHTML = '';
     document.getElementById('champs').value = '';
@@ -32,23 +87,27 @@ function zero() {
     document.getElementById('champs').focus();
 }
 
-document.getElementById('champs').addEventListener('keypress', (event) => {
+document.getElementById('champs').addEventListener('keydown', (event) => {
     const keyName = event.key;
+
     switch(keyName) {
         case 'Enter':
             combien();
             break;
+        case 'Delete':
+            zero();
+            break;
         default:
             console.log(keyName);
-            break;
     }
-});
+}, true);
 
 function demarrer() {
     document.getElementById('global').style.display = 'Block';
     document.getElementById('start').style.display = 'none';
     document.getElementById('champs').focus();
 }
+
 document.getElementById('btn').addEventListener('click', function() {
     combien();
 });
---
+++ script2.js
@@ -0,0 +1,46 @@
+function generateNumb() {
+    let tabTable = [];
+    let retVal;
+    if (document.getElementById('0').checked == true) {
+        tabTable.push('0');
+    }
+    if (document.getElementById('1').checked == true) {
+        tabTable.push('1');
+    }
+    if (document.getElementById('2').checked == true) {
+        tabTable.push('2');
+    }
+    if (document.getElementById('3').checked == true) {
+        tabTable.push('3');
+    }
+    if (document.getElementById('4').checked == true) {
+        tabTable.push('4');
+    }
+    if (document.getElementById('5').checked == true) {
+        tabTable.push('5');
+    }
+    if (document.getElementById('6').checked == true) {
+        tabTable.push('6');
+    }
+    if (document.getElementById('7').checked == true) {
+        tabTable.push('7');
+    }
+    if (document.getElementById('8').checked == true) {
+        tabTable.push('8');
+    }
+    if (document.getElementById('9').checked == true) {
+        tabTable.push('9');
+    }
+    if (document.getElementById('10').checked == true) {
+        tabTable.push('10');
+    }
+    if (tabTable.length > 0) {
+        retVal = Math.floor(Math.random() * tabTable.length);
+        retVal = parseInt(tabTable[retVal]);
+        console.log('éléments dans le tableau : ' + tabTable.length + ', sélectionné : ' + retVal + ', valeur : ' + tabTable[retVal]);
+    } else {
+        alert('Sélectionne au moins une table !');
+        retVal = 0;
+    }
+    return retVal;
+}
