8bf2cbce68497879686eaf6d0ff17c36515114e0
Gregory <gregory.caron1@gmail.com>
2018-12-01 23:18:27 +0100

    Corrections

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 let fNum;
 let sNum;
-
+function chaineEnBooleen(maChaine) {
+    /*maChaine = maChaine.toLowerCase();
+    maChaine = maChaine.trim();*/
+    switch(maChaine) {
+        case true:
+        case 'yes':
+        case 1:
+        case '1':
+            return true;
+        case false:
+        case 'no':
+        case 0:
+        case '0':
+        case null:
+            return false;
+        default:
+            return Boolean(string);
+    }
+}
 function iif(condition, trueCondition, falseCondition) {
     if (condition) {
         return trueCondition;
@@ -11,17 +29,17 @@ function iif(condition, trueCondition, falseCondition) {
 
 function loadPrefs() {
     if (typeof Storage !== 'undefined') {
-        localStorage.table0 = iif(document.getElementById('0').checked === true, true, false);
-        localStorage.table1 = iif(document.getElementById('1').checked === true, true, false);
-        localStorage.table2 = iif(document.getElementById('2').checked === true, true, false);
-        localStorage.table3 = iif(document.getElementById('3').checked === true, true, false);
-        localStorage.table4 = iif(document.getElementById('4').checked === true, true, false);
-        localStorage.table5 = iif(document.getElementById('5').checked === true, true, false);
-        localStorage.table6 = iif(document.getElementById('6').checked === true, true, false);
-        localStorage.table7 = iif(document.getElementById('7').checked === true, true, false);
-        localStorage.table8 = iif(document.getElementById('8').checked === true, true, false);
-        localStorage.table9 = iif(document.getElementById('9').checked === true, true, false);
-        localStorage.table10 = iif(document.getElementById('10').checked === true, true, false);
+        document.getElementById('0').checked = chaineEnBooleen(iif(localStorage.table0 === true, true, false));
+        document.getElementById('1').checked = chaineEnBooleen(iif(localStorage.table1 === true, true, false));
+        document.getElementById('2').checked = chaineEnBooleen(iif(localStorage.table2 === true, true, false));
+        document.getElementById('3').checked = chaineEnBooleen(iif(localStorage.table3 === true, true, false));
+        document.getElementById('4').checked = chaineEnBooleen(iif(localStorage.table4 === true, true, false));
+        document.getElementById('5').checked = chaineEnBooleen(iif(localStorage.table5 === true, true, false));
+        document.getElementById('6').checked = chaineEnBooleen(iif(localStorage.table6 === true, true, false));
+        document.getElementById('7').checked = chaineEnBooleen(iif(localStorage.table7 === true, true, false));
+        document.getElementById('8').checked = chaineEnBooleen(iif(localStorage.table8 === true, true, false));
+        document.getElementById('9').checked = chaineEnBooleen(iif(localStorage.table9 === true, true, false));
+        document.getElementById('10').checked = chaineEnBooleen(iif(localStorage.table10 === true, true, false));
     }
 }
 
@@ -46,48 +64,38 @@ function generateNumb() {
     let retVal;
     if (document.getElementById('0').checked === true) {
         tabTable.push(0);
-        savePrefs();
     }
     if (document.getElementById('1').checked === true) {
         tabTable.push(1);
-        savePrefs();
     }
     if (document.getElementById('2').checked === true) {
         tabTable.push(2);
-        savePrefs();
     }
     if (document.getElementById('3').checked === true) {
         tabTable.push(3);
-        savePrefs();
     }
     if (document.getElementById('4').checked === true) {
         tabTable.push(4);
-        savePrefs();
     }
     if (document.getElementById('5').checked === true) {
         tabTable.push(5);
-        savePrefs();
     }
     if (document.getElementById('6').checked === true) {
         tabTable.push(6);
-        savePrefs();
     }
     if (document.getElementById('7').checked === true) {
         tabTable.push(7);
-        savePrefs();
     }
     if (document.getElementById('8').checked === true) {
         tabTable.push(8);
-        savePrefs();
     }
     if (document.getElementById('9').checked === true) {
         tabTable.push(9);
-        savePrefs();
     }
     if (document.getElementById('10').checked === true) {
         tabTable.push(10);
-        savePrefs();
     }
+    savePrefs();
     if (tabTable.length > 0) {
         retVal = Math.floor(Math.random() * tabTable.length);
         console.log('éléments dans le tableau : ' + tabTable.length + ', sélectionné : ' + retVal + ', valeur : ' + parseInt(tabTable[retVal]));
@@ -151,6 +159,7 @@ document.getElementById('champs').addEventListener('keydown', (event) => {
             break;
         case 'Delete':
             zero();
+            loadPrefs();
             break;
         default:
             console.log(keyName);
